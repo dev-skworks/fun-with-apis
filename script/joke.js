27b8cb8294fc650f46232ce3598d14cd6d056ad5
Damian Padilla <damipad4@gmail.com>
2023-02-20 18:48:00 -0600

    init joke api

---
+++ script/joke.js
@@ -0,0 +1,15 @@
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+
+    try{
+        const response = await fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('.joke').innerText = data.joke
+
+    }catch(error){
+        console.log(error)
+    }
+}
