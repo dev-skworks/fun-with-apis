eacd0ea16e078597c4ba432dc565a2627744986a
Sukhdeep Singh <sukhdsv@gmail.com>
2023-02-21 16:06:58 -0800

    Adds normalize and reset CSS files. Adds space and dog API webpages.

---
+++ script/dog.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+
+  const url = `https://dog.ceo/api/breeds/image/random`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.message
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
