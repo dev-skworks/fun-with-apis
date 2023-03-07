79baef72f5f297a794b86c5d2af2a234631d1f76
Damian Padilla <damipad4@gmail.com>
2023-03-07 16:06:20 -0600

    feat: added cat api page

---
+++ script/cat.js
@@ -0,0 +1,18 @@
+
+
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+
+    try{
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_ke6CKuXZYiSmLzUMs5htvldGXZb9v4Xt31VpikeVv2IjiiWXfUzy6QKKzaAZDiXO`)
+
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('.cat-photo').src = data[0].url
+
+    }catch(error){
+        console.log(error)
+    }
+}
